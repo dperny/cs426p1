3a11a4d0a3f01a306f1a3d5b5a7baea9f93434ee
Drew Erny <drewerny@gmail.com>
2015-02-09 22:18:43 -0600

    finished first half of the project

--- osh.c
+++ osh.c
@@ -1,22 +1,68 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 
 #define MAX_LINE 80 /* maximum length command */
 
 int main(void) {
-  char* args[MAX_LINE/2 + 1]; /* command line arguments */
-  int should_run = 1; /* flag to determine when to exit program */
+  // command line arguments
+  char* args[MAX_LINE/2 + 1]; 
+  // a buffer to hold commands
+  char buffer[80];
+  // flag for exiting
+  int should_run = 1;
+  // flag for if we should wait
+  int should_wait = 1;
+
+  pid_t pid;
 
   while(should_run) {
-    printf("osh>");
+    // print the prompt
+    printf("osh> ");
     fflush(stdout);
 
-    /*
-     * After reading user input, the steps are:
-     * (1) fork a child process using fork()
-     * (2) the child process will invoke execvp()
-     * (3) if command included &, parent will invoke wait()
-     */
+    // read into the buffer. skip this entire section if the read fails
+    if(fgets(buffer, 79, stdin)) { 
+      
+      // parse the buffer into an arg array
+      int count = 0;
+      args[0] = strtok(buffer, " \n");
+      while(args[count] != NULL) {
+        count++;
+        args[count] = strtok(NULL, " \n");
+      }
+      
+      // skip this whole section if the first arg is null
+      // if we don't, it segfaults. hilarious
+      if(args[0] != NULL) {
+        if(!strcmp("exit", args[0])) should_run = 0; // exit if exit
+
+        // see if the last arg is a &
+        if(!strcmp(args[count-1], "&")) {
+          // if so, remove it and set the wait flag
+          should_wait = 1;
+          args[count-1] = NULL;
+        } else {
+          // otherwise, do not wait. just go
+          should_wait = 0;
+        }
+
+        // After reading user input, the steps are:
+        // (1) fork a child process using fork()
+        pid = fork();
+        if(!pid) {
+          // (2) the child process will invoke execvp()
+          execvp(args[0], args);
+        } else {
+          // (3) if command included &, parent will invoke wait()
+          if(should_wait) {
+            wait(NULL);
+          }
+        }
+
+      }
+    }
   }
 
   return 0;
